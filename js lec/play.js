6da5b44219b475d4f81c368fdf3283231f221545
Hawkeyecoder4 <162034853+Hawkeyecoder4@users.noreply.github.com>
2024-05-21 00:03:57 +0500

    Add files via upload

---
+++ js lec/play.js
@@ -0,0 +1,39 @@
+
+                                         "DATA TYPES"
+
+const numDate = '1,2,3,4,5';
+console.log(numDate);
+
+// in const data type we can not change the value of const varable data type //
+
+let num = '5,6,7,8,9,10';
+console.log(num);
+
+// in this data type we can change the value of let varibles data type
+
+var Data = '1,2,3,4,5,6';
+
+console.log(Data);
+// var is also the part of variable data type but its not recommanded because of var is sometime not working properly with scope{}
+
+let name = "UMAR" 
+    // data type is string
+let age = 15        
+//   data type is numeric
+let Isloggedin = true  
+// data type is boolian
+
+/*
+number    => we can use number 2 power 53.
+bigint    => use when number increase than 2 power of 53.
+string    => data type in "".
+boolian   => two value return (True \ False).
+null      => standalone set.(value assign empty) 
+undefined => value not assign.
+symbol    => use for to show something unique.
+
+
+
+object    => 
+
+*/
