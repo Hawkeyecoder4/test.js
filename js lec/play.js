99bea87213704c5faf069b656037cefa1199b208
Hawkeyecoder4 <162034853+Hawkeyecoder4@users.noreply.github.com>
2024-05-23 00:18:37 +0500

    Add files via upload

--- js lec/play.js
+++ js lec/play.js
@@ -93,3 +93,85 @@ console.log("1"+2+3);
 // if string first all values add like string 
 // if number first than first number add and than opreate string
 
+
+/*****************************************************Convertion********************************************** */
+/* in convertion result always show in boolian form  True or False */
+// simple Convertion
+console.log(2>3);
+console.log(2<3);
+console.log(2==3);
+console.log(2>=3);
+console.log(2<=3);
+
+/* some different type of data conversions */
+console.log("2">2);
+console.log(2=="2");
+console.log("2"==2);
+console.log(2==="2");
+
+
+/* different type of data convertion effect on result that doesnt give the predictable value
+Strictly prohabted to use different type of data for same convertion*/ 
+
+/*  "==" is used for equal but not strictly check data type otherwise "===" is also use for convertion but strictly check 
+data type if data is not same the result of "===" is false */
+
+
+/* There are two major types of data type
+ 1)  Primitive 
+ 2)  Non Primitive
+ 
+ Primitive data types are almost 7 further types
+ 1)  string       let name = "Umar";
+ 2)  Number       let age  =  24;
+ 3)  Boolean    let logged =  True;
+ 4)  Bigint       let num  =  123456789023345n
+ 5)  null         let tem  =  Null
+ 6)  undefined     let age;
+ 7)  symbol        use further next topics
+
+ non Premitive data types are almost 3 fruther types
+ 1)  Array           let Airmens = ["umar","hamza","Tanveer","Fasil","Saif"]
+ 2)  Object           let student = {
+                                name = "Umar";
+                                age  = 24;
+                            islogged = True
+                             }
+ 3)  Functions              Function(){}     =>  let addition = function(){
+                                                   console.log(2+3);
+                                                   }
+
+/*************************************************Memory Type*************************************************/
+/* There are two types of memory in js "stack" and "Heap"
+stack Memory : 
+              primitive data stored in stack memory
+Heap Memory  : 
+              Non Primitive data stored in heap memory
+
+             String and its methods 
+             let name =  "UmarAbbasJutt"  
+             => 'UmarAbbasJutt'  => Same in double and single qouts...
+                   */
+let name1 = "UmarAbbas";
+                    console.log( name1.length); 
+                    /* find length */
+                    console.log(name1.toUpperCase)
+                     /* to change uppercase and lowercase */
+                    console.log(name1.charAt(4))
+                     /*find position of character in string  */
+                    console.log(name1.indexOf('b'))
+                     /* find alphabet position  */
+                    console.log(name1[4])
+                     /* find character position */
+                    console.log(name1.substring(0,4))
+                     /* to find character length from to from */
+                    console.log(name.slice(-1 , 4));
+                     /*  slice use in negative or reverse*/
+                    console.log(name.replace('U' , 'O'));
+                     /* to Replace character */
+                    console.log(name.trim("Umar"));
+                     /* trim use to trim string Character search at GPT */
+                    console.log(name.split("A"));
+                     /* To change in String into Array*/
+
+                      /* Further more check prototype to check all fuctions and method about string */
